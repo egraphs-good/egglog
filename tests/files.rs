eef48a402aa110147e45f86313ec050ebb298fb6
Max Willsey <me@mwillsey.com>
2022-03-29 14:36:16 -0700

    Refactor tests

---
+++ tests/files.rs
@@ -0,0 +1,27 @@
+use egg_smol::*;
+use std::path::Path;
+
+fn walk_directory(path: &Path, f: &mut impl FnMut(&Path)) {
+    if path.is_file() {
+        f(path)
+    } else if path.is_dir() {
+        for entry in std::fs::read_dir(path).unwrap() {
+            let path = entry.unwrap().path();
+            walk_directory(&path, f);
+        }
+    } else {
+        panic!("Not a file or directory??")
+    }
+}
+
+#[test]
+fn test_files() {
+    walk_directory(Path::new("tests/"), &mut |path| {
+        if path.extension().unwrap_or_default() == "egg" {
+            println!("Running test {path:?}");
+            let program = std::fs::read_to_string(path).unwrap();
+            let mut egraph = EGraph::default();
+            egraph.run_program(&program);
+        }
+    });
+}
