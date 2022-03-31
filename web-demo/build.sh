c2b273c01efb516ba75d518eb4cd1c47162324aa
Max Willsey <me@mwillsey.com>
2022-03-31 14:00:41 -0700

    Try build again

--- web-demo/build.sh
+++ web-demo/build.sh
@@ -2,5 +2,9 @@
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
+# make sure to remove the .gitignore introduces by wasm-pack
+# so the deployment to github pages works
+
 set -ev
 wasm-pack build --target no-modules --no-typescript --out-dir www/pkg/
+rm -f www/pkg/.gitignore
