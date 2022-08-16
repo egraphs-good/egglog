7a6f1e074294632f88c8a28836c40d1afe7b7c2a
Max Willsey <me@mwillsey.com>
2022-08-16 14:14:41 -0700

    Make deterministic by bumping symbol_table

--- src/util.rs
+++ src/util.rs
@@ -5,8 +5,7 @@ use std::fmt::Display;
 #[allow(unused_imports)]
 use crate::*;
 
-pub(crate) type BuildHasher = ahash::RandomState;
-pub(crate) const BUILD_HASHER: BuildHasher = BuildHasher::with_seeds(0, 0, 0, 0);
+pub(crate) type BuildHasher = std::hash::BuildHasherDefault<rustc_hash::FxHasher>;
 
 pub(crate) type HashMap<K, V> = hashbrown::HashMap<K, V, BuildHasher>;
 pub(crate) type HashSet<K> = hashbrown::HashSet<K, BuildHasher>;
