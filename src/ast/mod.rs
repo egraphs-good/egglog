2c8f5513b9f17121e60a43641513404e0c00e77e
Philip Zucker <philzook58@gmail.com>
2022-12-08 00:35:22 -0500

    calc command

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -79,6 +79,7 @@ pub enum Command {
     Include(String),
 }
 #[derive(Clone, Debug)]
+<<<<<<< HEAD
 pub struct IdentSort {
     pub ident: Symbol,
     pub sort: Symbol,
@@ -89,6 +90,12 @@ impl Display for IdentSort {
         write!(f, "({} {})", self.ident, self.sort)
     }
 }
+=======
+pub struct TypeBind {
+    pub ident: Symbol,
+    pub sort: Symbol,
+}
+>>>>>>> 850cd71 (calc command)
 
 #[derive(Clone, Debug)]
 pub struct RunConfig {
