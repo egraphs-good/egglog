04bf2761dce38c6e78689d12033b79d969c9b9ce
oflatt <oflatt@gmail.com>
2023-01-09 13:53:29 -0800

    a bunch of eval functions for f64

--- src/sort/f64.rs
+++ src/sort/f64.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use crate::ast::Literal;
 
 use super::*;
@@ -64,6 +66,87 @@ impl Sort for F64Sort {
         add_primitives!(eg, "dist" = |a: F64, b: F64| -> F64 {
             OrderedFloat((a - b).abs())
         });
+        
+        add_primitives!(eg, "f64-PI" = | | -> F64 {
+            OrderedFloat(PI)
+        });
+        add_primitives!(eg, "f64-E" = | | -> F64 {
+            OrderedFloat(std::f64::consts::E)
+        });
+        add_primitives!(eg, "f64-INFINITY" = | | -> F64 {
+            OrderedFloat(std::f64::INFINITY)
+        });
+
+        add_primitives!(eg, "f64-Neg" = |a: F64| -> F64 {
+            -a
+        });
+
+        add_primitives!(eg, "f64-Sqrt" = |a: F64| -> F64 {
+            OrderedFloat(a.sqrt())
+        });
+        add_primitives!(eg, "f64-Cbrt" = |a: F64| -> F64 {
+            OrderedFloat(a.cbrt())
+        });
+        add_primitives!(eg, "f64-Fabs" = |a: F64| -> F64 {
+            OrderedFloat(a.abs())
+        });
+        add_primitives!(eg, "f64-Ceil" = |a: F64| -> F64 {
+            OrderedFloat(a.ceil())
+        });
+        add_primitives!(eg, "f64-Floor" = |a: F64| -> F64 {
+            OrderedFloat(a.floor())
+        });
+        add_primitives!(eg, "f64-Round" = |a: F64| -> F64 {
+            OrderedFloat(a.round())
+        });
+        add_primitives!(eg, "f64-Log" = |a: F64| -> F64 {
+            OrderedFloat(a.ln())
+        });
+        add_primitives!(eg, "f64-Exp" = |a: F64| -> F64 {
+            OrderedFloat(a.exp())
+        });
+        add_primitives!(eg, "f64-Sin" = |a: F64| -> F64 {
+            OrderedFloat(a.sin())
+        });
+        add_primitives!(eg, "f64-Cos" = |a: F64| -> F64 {
+            OrderedFloat(a.cos())
+        });
+        add_primitives!(eg, "f64-Tan" = |a: F64| -> F64 {
+            OrderedFloat(a.tan())
+        });
+        add_primitives!(eg, "f64-Asin" = |a: F64| -> F64 {
+            OrderedFloat(a.asin())
+        });
+        add_primitives!(eg, "f64-Acos" = |a: F64| -> F64 {
+            OrderedFloat(a.acos())
+        });
+        add_primitives!(eg, "f64-Atan" = |a: F64| -> F64 {
+            OrderedFloat(a.atan())
+        });
+        add_primitives!(eg, "f64-Sinh" = |a: F64| -> F64 {
+            OrderedFloat(a.sinh())
+        });
+        add_primitives!(eg, "f64-Cosh" = |a: F64| -> F64 {
+            OrderedFloat(a.cosh())
+        });
+        add_primitives!(eg, "f64-Tanh" = |a: F64| -> F64 {
+            OrderedFloat(a.tanh())
+        });
+        add_primitives!(eg, "f64-Expm1" = |a: F64| -> F64 {
+            OrderedFloat(a.exp_m1())
+        });
+        add_primitives!(eg, "f64-Log1p" = |a: F64| -> F64 {
+            OrderedFloat(a.ln_1p())
+        });
+
+        add_primitives!(eg, "f64-Add" = |a: F64, b: F64| -> F64 { a + b });
+        add_primitives!(eg, "f64-Sub" = |a: F64, b: F64| -> F64 { a - b });
+        add_primitives!(eg, "f64-Mul" = |a: F64, b: F64| -> F64 { a * b });
+        add_primitives!(eg, "f64-Div" = |a: F64, b: F64| -> F64 { a / b });
+        add_primitives!(eg, "f64-Pow" = |a: F64, b: F64| -> F64 { OrderedFloat(a.into_inner().powf(b.into_inner())) });
+        add_primitives!(eg, "f64-Atan2" = |a: F64, b: F64| -> F64 { OrderedFloat(a.into_inner().atan2(b.into_inner())) });
+        add_primitives!(eg, "f64-Hypot" = |a: F64, b: F64| -> F64 { OrderedFloat(a.into_inner().hypot(b.into_inner())) });
+        add_primitives!(eg, "f64-Fma" = |a: F64, b: F64, c: F64| -> F64 { OrderedFloat(a.into_inner().mul_add(b.into_inner(), c.into_inner())) });
     }
 
     fn make_expr(&self, value: Value) -> Expr {
