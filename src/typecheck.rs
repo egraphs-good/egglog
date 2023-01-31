8289990880eba82087acd8c03a3c09e13f61f1ac
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-01-31 11:41:42 -0800

    Fix: Detect saturation when `set` is a noop

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -734,14 +734,13 @@ impl EGraph {
                     // We should only have canonical values here: omit the canonicalization step
                     let old_value = function.insert(args, new_value, self.timestamp);
 
-                    // if the value does not exist or the two values differ
-                    if old_value.is_none() || old_value != Some(new_value) {
+                    // if the value does not exist
+                    if old_value.is_none() {
                         self.saturated = false;
                     }
 
                     if let Some(old_value) = old_value {
                         if new_value != old_value {
-                            self.saturated = false;
                             let tag = old_value.tag;
                             if let Some(prog) = function.merge.on_merge.clone() {
                                 let values = [old_value, new_value];
@@ -754,9 +753,11 @@ impl EGraph {
                             let function = self.functions.get_mut(f).unwrap();
                             let merged: Value = match function.merge.merge_vals.clone() {
                                 MergeFn::AssertEq => {
+                                    self.saturated = false;
                                     return Err(Error::MergeError(*f, new_value, old_value))
                                 }
                                 MergeFn::Union => {
+                                    self.saturated = false;
                                     self.unionfind.union_values(old_value, new_value, tag)
                                 }
                                 MergeFn::Expr(merge_prog) => {
@@ -765,6 +766,7 @@ impl EGraph {
                                     self.run_actions(stack, &values, &merge_prog, true)?;
                                     let result = stack.pop().unwrap();
                                     stack.truncate(old_len);
+                                    self.saturated &= result == old_value;
                                     result
                                 }
                             };
