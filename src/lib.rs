aa84ba2e910b485766ae3ae3f6489baf75b789c9
oflatt <oflatt@gmail.com>
2023-02-01 15:20:07 -0800

    oops

--- src/lib.rs
+++ src/lib.rs
@@ -682,7 +682,7 @@ impl EGraph {
         let (query0, action0) = ctx
             .typecheck_query(&rule.body, &rule.head)
             .map_err(Error::TypeErrors)?;
-        let query = self.compile_gj_query(query0.clone(), &ctx.types);
+        let query = self.compile_gj_query(query0, &ctx.types);
         let program = self
             .compile_actions(&ctx.types, &action0)
             .map_err(Error::TypeErrors)?;
