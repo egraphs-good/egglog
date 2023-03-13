27a8ad4e9e6a7f3299cce3332ba37b307b0d8469
Max Willsey <me@mwillsey.com>
2023-03-13 15:45:30 -0700

    Fix not running tests

--- build.rs
+++ build.rs
@@ -1,29 +1,46 @@
 use std::env;
+use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/ast/parse.lalrpop");
     lalrpop::process_root().unwrap();
     let mdir = std::env!("CARGO_MANIFEST_DIR");
-    generate_tests(&format!("{mdir}/tests/*.egg"), false);
-    generate_tests(&format!("{mdir}/tests/fail-typecheck/*.egg"), true);
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let test_file = &mut File::create(format!("{out_dir}/files.rs")).unwrap();
+    generate_tests(test_file, &format!("{mdir}/tests/**/*.egg"));
 }
 
-fn generate_tests(glob: &str, should_fail: bool) {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let mut out = std::fs::File::create(format!("{out_dir}/files.rs")).unwrap();
-    for f in glob::glob(glob).unwrap() {
-        let f = f.unwrap();
+fn generate_tests(file: &mut File, glob: &str) {
+    let paths: Vec<PathBuf> = glob::glob(glob)
+        .unwrap()
+        .collect::<Result<_, glob::GlobError>>()
+        .unwrap();
+
+    writeln!(file, "const N_TEST_FILES: usize = {};", paths.len()).unwrap();
+
+    for f in paths {
         let name = f
             .file_stem()
             .unwrap()
             .to_string_lossy()
             .replace(['.', '-', ' '], "_");
+
+        let should_fail = f.to_string_lossy().contains("fail-typecheck");
+
         // write a normal test
         writeln!(
-            out,
-            "#[test] fn {name}() {{ run({:?}, false, {}); }}",
-            f, should_fail
+            file,
+            r#" #[test] 
+            fn {name}() {{ 
+                Run {{
+                    path: {f:?},
+                    should_fail: {should_fail},
+                    test_proofs: false,
+                }}.run(); 
+            }}"#,
         )
         .unwrap();
 
@@ -31,9 +48,15 @@ fn generate_tests(glob: &str, should_fail: bool) {
         // TODO: re-enable herbie, unsound, and eqsolve when proof extraction is faster
         if !(name == "herbie" || name == "repro_unsound" || name == "eqsolve") {
             writeln!(
-                out,
-                "#[test] fn {name}_with_proofs() {{ run({:?}, true, {}); }}",
-                f, should_fail
+                file,
+                r#" #[test] 
+                fn {name}_with_proofs() {{ 
+                    Run {{
+                        path: {f:?},
+                        should_fail: {should_fail},
+                        test_proofs: true,
+                    }}.run(); 
+                }}"#,
             )
             .unwrap();
         }
