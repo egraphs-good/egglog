e082fba964bdb8a93780f5b6afc5deb9cbda280f
oflatt <oflatt@gmail.com>
2023-05-08 10:53:22 -0700

    not working due to cycles lol

--- scripts/terms.rkt
+++ scripts/terms.rkt
@@ -8,20 +8,29 @@
       (Sub Math Math)
       (Mul Math Math)
       (Div Math Math)
-      (Const Rational)
+      (Const i64)
       (Var String))
 
-    (function Parent (Math) Math :merge new)
+    (relation Parent (Math Math))
 
     ;; rebuilding rules
+    (ruleset parent-subsume)
+    (rule ((Parent a a)
+          (Parent a c)
+          (!= a c))
+        ((delete (Parent a a)))
+        :ruleset parent-subsume)
+
     (ruleset parent)
     (rule ((Parent a b)
           (Parent b c))
-          ((Parent a c)))
+          ((Parent a c))
+          :ruleset parent)
 
     (ruleset rebuilding1)
 
     ;; make the left child canonical and add it to the eclass
+    ;; TODO subsume the old one
     ,@(for/list ([op binary])
        `(rule ((= e (,op a b))
               (Parent e ep)
@@ -42,7 +51,44 @@
               :ruleset rebuilding2))
 
 
-    ;; rules   
+    ;; rules
+    (ruleset just-add)
+    (ruleset do-union)
+
+    ;; commutativity of addition
+    (rule ((Add a b))
+          ((let rhs (Add b a))
+           (Parent rhs rhs))
+          :ruleset just-add)
+    (rule ((= lhs (Add a b))
+           (= rhs (Add b a))
+           (Parent lhs p1)
+           (Parent rhs p2)
+           (!= p1 p2)
+           )
+          ;; set lhs parent to rhs parent
+          ((Parent p1 p2))
+          :ruleset do-union)
+
+    (let a (Add (Const 1) (Const 2)))
+
+
+    ;; time to run!
+    (run-schedule
+      (repeat 1
+        (saturate parent-subsume)
+        (saturate parent)
+        (saturate rebuilding1)
+        (saturate rebuilding2)
+        (saturate just-add)
+        (saturate parent-subsume)
+        (saturate do-union)))
+    
+    (run-schedule (saturate parent-subsume))
+
+    (let b (Add (Const 2) (Const 1)))
+
+    (check (Parent a p1) (Parent b p2) (= p1 p2))
   ))
 
 
