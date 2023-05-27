ddad72fa60a0017042b5b5092d91603fdf242498
Saul Shanabrook <s.shanabrook@gmail.com>
2023-05-27 16:32:09 -0400

    Switch to using macros for graphviz gen to make smaller

--- src/graph/to_graphviz.rs
+++ src/graph/to_graphviz.rs
@@ -1,26 +1,28 @@
 use super::*;
-use graphviz_rust::{attributes as a, dot_structures as d};
+use graphviz_rust::{
+    attributes::*,
+    dot_generator::*,
+    dot_structures::{
+        Edge, EdgeTy, Graph, GraphAttributes as GA, Id, Node, NodeId, Stmt, Subgraph, Vertex,
+    },
+};
 
 /// Implement conversion of Graph to graphviz Graph
 impl ExportedGraph {
-    pub fn to_graphviz(&self) -> d::Graph {
+    pub fn to_graphviz(&self) -> Graph {
         let mut statements = vec![
             // Set to compound so we can have edge to clusters
-            d::Stmt::Attribute(a::GraphAttributes::compound(true)),
+            stmt!(GraphAttributes::compound(true)),
             // Set default sub-graph rank to be same so that all nodes in e-class are on same level
-            d::Stmt::Attribute(a::SubgraphAttributes::rank(a::rank::same)),
-            d::Stmt::Attribute(a::GraphAttributes::fontname("helvetica".to_string())),
-            d::Stmt::Attribute(a::GraphAttributes::style(quote(
-                "rounded,dashed".to_string(),
-            ))),
-            d::Stmt::GAttribute(d::GraphAttributes::Edge(vec![
-                a::EdgeAttributes::arrowsize(0.5),
-            ])),
-            d::Stmt::GAttribute(d::GraphAttributes::Node(vec![
-                a::NodeAttributes::shape(a::shape::box_),
-                a::NodeAttributes::style("rounded".to_string()),
-                a::NodeAttributes::width(0.4),
-                a::NodeAttributes::height(0.4),
+            stmt!(SubgraphAttributes::rank(rank::same)),
+            stmt!(GraphAttributes::fontname("helvetica".to_string())),
+            stmt!(GraphAttributes::style("rounded".to_string())),
+            stmt!(GA::Edge(vec![EdgeAttributes::arrowsize(0.5)])),
+            stmt!(GA::Node(vec![
+                NodeAttributes::shape(shape::box_),
+                NodeAttributes::style("rounded".to_string()),
+                NodeAttributes::width(0.4),
+                NodeAttributes::height(0.4),
             ])),
         ];
         statements.extend(
@@ -33,62 +35,45 @@ impl ExportedGraph {
                 eclass_to_graphviz(eclass_id, eclass, &self.eclasses)
             }),
         );
-        d::Graph::DiGraph {
-            id: d::Id::Plain("egg_smol".to_string()),
-            strict: false,
-            stmts: statements,
-        }
+        graph!(di id!(), statements)
     }
 }
 
 /// An e-class is converted into a cluster with a node for each function call
-fn eclass_to_graphviz(
-    eclass_id: &EClassID,
-    fn_calls: &[FnCall],
-    eclasses: &EClasses,
-) -> Vec<d::Stmt> {
+fn eclass_to_graphviz(eclass_id: &EClassID, fn_calls: &[FnCall], eclasses: &EClasses) -> Vec<Stmt> {
     // Create node for all arguments of every function call
-    let mut stmts: Vec<d::Stmt> = fn_calls
+    let mut stmts: Vec<Stmt> = fn_calls
         .iter()
         .flat_map(|fn_call| {
             fn_call
                 .1
                 .iter()
                 .flat_map(|arg| arg.to_graphviz(fn_call_id(fn_call), eclasses))
-                .collect::<Vec<d::Stmt>>()
+                .collect::<Vec<Stmt>>()
         })
         .collect();
     // Add a node for each function call in one e-class
-    stmts.push(d::Stmt::Subgraph(d::Subgraph {
-        id: d::Id::Plain(cluster_name(eclass_id)),
-        // Nest in empty sub-graph so that we can use rank=same
-        // https://stackoverflow.com/a/55562026/907060
-        stmts: vec![d::Stmt::Subgraph(d::Subgraph {
-            id: d::Id::Plain("".to_string()),
-            stmts: fn_calls
-                .iter()
-                .map(|fn_call| {
-                    d::Stmt::Node(d::Node::new(
-                        d::NodeId(d::Id::Plain(quote(fn_call_id(fn_call))), None),
-                        label_attributes(fn_call.0.name.clone()),
-                    ))
-                })
-                .collect(),
-        })],
-    }));
+    let subgraph_stmts = fn_calls
+        .iter()
+        .map(|fn_call| {
+            let id = fn_call_id(fn_call);
+            stmt!(node!(esc id; NodeAttributes::label(quote(fn_call.0.name.clone()))))
+        })
+        .collect::<Vec<Stmt>>();
+    let cluster_id = cluster_name(eclass_id);
+    // Nest in empty sub-graph so that we can use rank=same
+    // https://stackoverflow.com/a/55562026/907060
+    stmts.push(stmt!(subgraph!(cluster_id; subgraph!("", subgraph_stmts))));
     stmts
 }
 
 impl PrimOutput {
     /// A primitive output, should be a node with the value and function call
-    fn to_graphviz(&self, eclasses: &EClasses) -> Vec<d::Stmt> {
-        let mut stmts = Vec::new();
+    fn to_graphviz(&self, eclasses: &EClasses) -> Vec<Stmt> {
         let label = format!("{}: {}", self.0 .0.name, self.1.to_string());
         let res_id = fn_call_id(&self.0);
-        stmts.push(d::Stmt::Node(d::Node::new(
-            d::NodeId(d::Id::Plain(quote(res_id.clone())), None),
-            label_attributes(label),
-        )));
+        let node_id = quote(res_id.clone());
+        let mut stmts = vec![stmt!(node!(node_id; NodeAttributes::label(quote(label))))];
         stmts.extend(
             self.0
                  .1
@@ -103,45 +88,26 @@ impl Arg {
     /// Returns an edge from the result to the argument
     /// If it's an e-class, use the e-class-id as the target
     /// Otherwise, create a node for the primitive value and use that as the target
-    fn to_graphviz(&self, result_id: String, eclasses: &EClasses) -> Vec<d::Stmt> {
-        let result_node = d::Vertex::N(d::NodeId(d::Id::Plain(quote(result_id.clone())), None));
+    fn to_graphviz(&self, result_id: String, eclasses: &EClasses) -> Vec<Stmt> {
+        let result_name = quote(result_id.clone());
         match self {
             Arg::Prim(p) => {
-                let arg_id = d::NodeId(d::Id::Plain(quote(prim_value_id(result_id, p))), None);
+                let arg_id = quote(prim_value_id(result_id, p));
                 vec![
-                    d::Stmt::Node(d::Node::new(
-                        arg_id.clone(),
-                        label_attributes(p.to_string()),
-                    )),
-                    d::Stmt::Edge(d::Edge {
-                        ty: d::EdgeTy::Pair(result_node, d::Vertex::N(arg_id)),
-                        attributes: vec![],
-                    }),
+                    stmt!(node!(arg_id; NodeAttributes::label(quote(p.to_string())))),
+                    stmt!(edge!(node_id!(result_name) => node_id!(arg_id))),
                 ]
             }
             Arg::Eq(id) => {
-                vec![d::Stmt::Edge(d::Edge {
-                    ty: d::EdgeTy::Pair(
-                        result_node,
-                        d::Vertex::N(d::NodeId(
-                            d::Id::Plain(quote(enode_fn_id(id, eclasses))),
-                            None,
-                        )),
-                    ),
-                    attributes: vec![graphviz_rust::attributes::EdgeAttributes::lhead(
-                        cluster_name(id),
-                    )],
-                })]
+                vec![stmt!(edge!(
+                    node_id!(result_name) => node_id!(quote(enode_fn_id(id, eclasses)));
+                    EdgeAttributes::lhead(cluster_name(id))
+                ))]
             }
         }
     }
 }
 
-fn label_attributes(label: String) -> Vec<d::Attribute> {
-    vec![graphviz_rust::attributes::NodeAttributes::label(quote(
-        label,
-    ))]
-}
 fn quote(s: String) -> String {
     format!("{:?}", s)
 }
