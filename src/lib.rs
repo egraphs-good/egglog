c9de3d58d614b5ac675140e7291cf60c662ac5d2
Saul Shanabrook <s.shanabrook@gmail.com>
2023-06-12 10:30:25 -0400

    Add back accidentally remove assert

--- src/lib.rs
+++ src/lib.rs
@@ -273,6 +273,7 @@ impl EGraph {
                     .iter()
                     .chain(once(&function.schema.output)),
             ) {
+                assert!(sort.is_eq_container_sort() == rix.is_some());
                 if sort.is_eq_container_sort() {
                     let rix = rix.as_ref().unwrap();
                     for ix in rix.iter() {
