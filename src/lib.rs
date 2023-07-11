b49fad8c0c61ae1109034fc48638b85803d32d13
Max Willsey <me@mwillsey.com>
2023-07-11 15:54:36 -0700

    Neaten after merge

--- src/lib.rs
+++ src/lib.rs
@@ -1026,7 +1026,7 @@ impl EGraph {
             0 => vec![],
             1 => vec![expr.clone()],
             _ => {
-                if Option::map_or(self.get_sort(&value), false, |sort| sort.is_eq_sort()) {
+                if self.get_sort(&value).is_some_and(|sort| sort.is_eq_sort()) {
                     self.extract_variants(value, variants)
                 } else {
                     vec![expr.clone()]
