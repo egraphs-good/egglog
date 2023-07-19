9fe03ad35a2a975a2c9140a641ba91266b7a72ce
Max Willsey <me@mwillsey.com>
2023-07-19 12:57:36 -0700

    Neaten test generation

--- tests/files.rs
+++ tests/files.rs
@@ -7,16 +7,15 @@ use libtest_mimic::Trial;
 struct Run {
     path: PathBuf,
     test_proofs: bool,
-    should_fail: bool,
 }
 
 impl Run {
     fn run(&self) {
         let _ = env_logger::builder().is_test(true).try_init();
-        let program = std::fs::read_to_string(&self.path).unwrap();
+        let program = std::fs::read_to_string(&self.path)
+            .unwrap_or_else(|err| panic!("Couldn't read {:?}: {:?}", self.path, err));
         self.test_program(&program, "Top level error");
-
-        if !self.should_fail {
+        if !self.should_fail() {
             let mut egraph = EGraph::default();
             egraph.set_underscores_for_desugaring(4);
             let parsed = egraph.parse_program(&program).unwrap();
@@ -49,7 +48,7 @@ impl Run {
         }
         match egraph.parse_and_run_program(program) {
             Ok(msgs) => {
-                if self.should_fail {
+                if self.should_fail() {
                     panic!(
                         "Program should have failed! Instead, logged:\n {}",
                         msgs.join("\n")
@@ -61,41 +60,54 @@ impl Run {
                 }
             }
             Err(err) => {
-                if !self.should_fail {
+                if !self.should_fail() {
                     panic!("{}: {err}", message)
                 }
             }
+        };
+    }
+
+    fn into_trial(self) -> Trial {
+        let name = self.name().to_string();
+        Trial::test(name, move || {
+            self.run();
+            Ok(())
+        })
+    }
+
+    fn name(&self) -> impl std::fmt::Display + '_ {
+        struct Wrapper<'a>(&'a Run);
+        impl std::fmt::Display for Wrapper<'_> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let stem = self.0.path.file_stem().unwrap();
+                let stem_str = stem.to_string_lossy().replace(['.', '-', ' '], "_");
+                write!(f, "{stem_str}")?;
+                if self.0.test_proofs {
+                    write!(f, "_with_proofs")?;
+                }
+                Ok(())
+            }
         }
+        Wrapper(self)
+    }
+
+    fn should_fail(&self) -> bool {
+        self.path.to_string_lossy().contains("fail-typecheck")
     }
 }
 
 fn generate_tests(glob: &str) -> Vec<Trial> {
     let mut trials = vec![];
-    let mut mk_trial = |name: String, run: Run| {
-        trials.push(Trial::test(name, move || {
-            run.run();
-            Ok(())
-        }))
-    };
+    let mut push_trial = |run: Run| trials.push(run.into_trial());
 
     for entry in glob::glob(glob).unwrap() {
-        let f = entry.unwrap();
-        let name = f
-            .file_stem()
-            .unwrap()
-            .to_string_lossy()
-            .replace(['.', '-', ' '], "_");
+        let run = Run {
+            path: entry.unwrap().clone(),
+            test_proofs: false,
+        };
+        let name = run.name().to_string();
 
-        let should_fail = f.to_string_lossy().contains("fail-typecheck");
-
-        mk_trial(
-            name.clone(),
-            Run {
-                path: f.clone(),
-                should_fail,
-                test_proofs: false,
-            },
-        );
+        push_trial(run.clone());
 
         // make a test with proofs enabled
         // TODO: re-enable herbie, unsound, and eqsolve when proof extraction is faster
@@ -107,14 +119,10 @@ fn generate_tests(glob: &str) -> Vec<Trial> {
             "lambda",
         ];
         if !banned.contains(&name.as_str()) {
-            mk_trial(
-                format!("{name}_with_proofs"),
-                Run {
-                    path: f.clone(),
-                    should_fail,
-                    test_proofs: true,
-                },
-            );
+            push_trial(Run {
+                test_proofs: true,
+                ..run
+            });
         }
     }
 
