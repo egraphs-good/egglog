d1aabca2b61a9b4d1c547c63df09b707433a9a62
hatoo <hato2000@gmail.com>
2023-11-30 14:38:24 +0900

    remove tree-sitter-egglog/build

--- vscode/egglog-language-server/tree-sitter-egglog/build/Release/.deps/Release/tree_sitter_egglog_binding.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/tree_sitter_egglog_binding.node := ln -f "Release/obj.target/tree_sitter_egglog_binding.node" "Release/tree_sitter_egglog_binding.node" 2>/dev/null || (rm -rf "Release/tree_sitter_egglog_binding.node" && cp -af "Release/obj.target/tree_sitter_egglog_binding.node" "Release/tree_sitter_egglog_binding.node")
