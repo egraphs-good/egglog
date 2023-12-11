a735990c8a1c29a384163432bd3886bf92642695
Yihong Zhang <yz489@cs.washington.edu>
2023-12-11 01:02:29 -0800

    tweak

--- src/lib.rs
+++ src/lib.rs
@@ -1102,7 +1102,7 @@ impl EGraph {
             &mut Default::default(),
             &mut ResolvedGen::new(),
         )?;
-        let target = mapped_expr.get_corresponding_var_or_lit(&self.type_info());
+        let target = mapped_expr.get_corresponding_var_or_lit(self.type_info());
         let program = self
             .compile_expr(&Default::default(), &actions, &target)
             .map_err(Error::TypeErrors)?;
