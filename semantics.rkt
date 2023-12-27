ff90ad3e8a49a80654d83e2f1986d99fec87173e
oflatt <oflatt@gmail.com>
2023-12-26 20:52:37 -0600

    typed judgement

--- semantics.rkt
+++ semantics.rkt
@@ -33,13 +33,15 @@
    (Term ...)]
   [Term number
         (constructor Term ...)]
-  [Program+Database
-   (Program Database)]
   [Command+Database
    (cmd Database)]
-  [Env (Assign ...)]
-  [Assign (var -> Term)]
-  )
+  [Env (Binding ...)]
+  [Binding (var -> Term)]
+  [TypeEnv (TypeBinding ...)]
+  [TypeBinding (var : Type)]
+  ;; TODO add types- right now we just
+  ;; check there are no free variables
+  [Type no-type])
 
 ;; No rule for if the variable is not found
 ;; Therefore it fails in such cases
@@ -56,9 +58,9 @@
   Database-Union : Database ... -> Database
   [(Database-Union Database)
    Database]
-  [(Database-Union ((Term_s ...) (Eq_s ...) (Assign_s ...)) Database_s ...)
-   ((Term_s ... Term_s2 ...) (Eq_s ... Eq_s2 ...) (Assign_s ... Assign_s2 ...))
-   (where ((Term_s2 ...) (Eq_s2 ...) (Assign_s2 ...))
+  [(Database-Union ((Term_s ...) (Eq_s ...) (Binding_s ...)) Database_s ...)
+   ((Term_s ... Term_s2 ...) (Eq_s ... Eq_s2 ...) (Binding_s ... Binding_s2 ...))
+   (where ((Term_s2 ...) (Eq_s2 ...) (Binding_s2 ...))
           (Database-Union Database_s ...))])
 
 
@@ -73,7 +75,9 @@
          (((constructor Term_c ...)) () ())
          Database_c ...))
    (where ((Term_c Database_c) ...)
-          ((Eval-Expr expr_s Env) ...))])
+          ((Eval-Expr expr_s Env) ...))]
+  [(Eval-Expr var Env)
+   ((Lookup var Env) (((Lookup var Env)) () ()))])
 
 
 (define (dump-pict pict name)
@@ -85,11 +89,10 @@
 
 (define-metafunction Egglog+Database
     Eval-Action : action Database -> Database
-    [(Eval-Action (let var expr) (Terms Congr Env))
-     ((var -> Term)
-      (Database-Union (Terms Congr Env) Database_2))
+    [(Eval-Action (let var expr) (Terms Congr (Binding_s ...)))
+     (Database-Union (Terms Congr ((var -> Term) Binding_s ...)) Database_2)
      (where (Term Database_2)
-            (Eval-Expr expr Env))]
+            (Eval-Expr expr (Binding_s ...)))]
     [(Eval-Action expr (Terms Congr Env))
      (Database-Union (Terms Congr Env) Database_2)
      (where (Term Database_2)
@@ -102,13 +105,18 @@
    ;; running actions
    (-->
     (action Database)
-    (skip Database))))
+    (skip (Eval-Action action Database)))))
 
+(define (single-element-or-false lst)
+  (match lst
+    [`(,x) x]
+    [`() #f]
+    [_ (error "Expected single element, got ~a" lst)]))
 
 (define Egglog-Reduction
   (reduction-relation
    Egglog+Database
-   #:domain Program+Database
+   #:domain (Program Database)
    (-->
     ((cmd_1 cmd_s ...)
      Database)
@@ -116,7 +124,7 @@
      Database_2)
     (where
      (cmd_stepped Database_2)
-     ,(first
+     ,(single-element-or-false
        (apply-reduction-relation
         Command-Reduction
         (term (cmd_1 Database))))))
@@ -125,6 +133,69 @@
      Database)
     ((cmd_s ...)
      Database))))
+
+
+(define-judgment-form
+  Egglog+Database
+  #:contract (typed-expr expr TypeEnv)
+  #:mode (typed-expr I I)
+  [----------------------------
+   (typed-expr number TypeEnv)]
+  [(side-condition ,(member (term (var : no-type))
+                           (term TypeEnv)))
+   ----------------------------
+   (typed-expr var TypeEnv)]
+  [(typed-expr expr_s TypeEnv) ...
+   ----------------------------
+   (typed-expr (constructor expr_s ...) TypeEnv)])
+
+
+(define-judgment-form
+  Egglog+Database
+  ;; Types an action in a given environment and
+  ;; returns the resulting environment
+  #:contract (typed-action action TypeEnv TypeEnv)
+  #:mode (typed-action I I O)
+  [(side-condition ,(not
+                     (member 
+                      (term (TypeBinding ...))
+                      (term (TypeBinding ...)))))
+   (typed-expr expr (TypeBinding ...))
+   ----------------------------
+   (typed-action (let var expr)
+                 (TypeBinding ...)
+                 ((var : no-type)
+                  TypeBinding ...))]
+  [(typed-expr expr TypeEnv)
+   ----------------------------
+   (typed-action expr TypeEnv TypeEnv)])
+
+
+(define-judgment-form
+  Egglog+Database
+  #:contract (typed Program TypeEnv)
+  #:mode (typed I O)
+  [---------------------------
+   (typed () ())]
+  [(typed (cmd_p ...) TypeEnv)
+   (typed-action action TypeEnv TypeEnv_res)
+   ---------------------------
+   (typed (cmd_p ... action) TypeEnv_res)])
+
+(define (types? e)
+  (judgment-holds (typed ,e TypeEnv)))
+
+(define (executes? prog)
+  (cond
+    [(not (types? prog))
+     #t]
+    [else
+     (define res
+      (apply-reduction-relation* Egglog-Reduction (term (,prog (() () ())))))
+     (match res
+      [`((() ,database))
+        #t]
+      [_ #f])]))
 
 
 (define-syntax-rule (save-metafunction func ...)
@@ -140,7 +211,18 @@
 
 (save-semantics)
 
+
 (module+ test
+  (check-false
+   (judgment-holds (typed-expr a ())))
+  (check-false
+   (judgment-holds
+     (typed (a) TypeEnv)))
+  (check-not-false
+   (judgment-holds
+    (typed ((let a 2) a)
+           TypeEnv)))
+
   (check-not-false
    (redex-match Egglog
                 Program
@@ -150,7 +232,7 @@
   (check-not-false
    (redex-match
     Egglog+Database
-    Program+Database
+    (Program Database)
     (term
      ((skip) (() () ())))))
 
@@ -167,7 +249,7 @@
     Command-Reduction
     (term
      ((Add 2 3) (() () ()))))
-   (list (term (skip (() () ())))))
+   (list (term (skip (((Add 2 3) 2 3) () ())))))
 
   (check-equal?
    (term (Eval-Expr 2 ()))
@@ -175,6 +257,11 @@
   (check-equal?
    (term (Eval-Expr (Add 2 3) ()))
    (term ((Add 2 3) (((Add 2 3) 2 3) () ()))))
+
+  (redex-check Egglog
+    Program
+    (executes? (term Program))
+    #:attempts 100000)
   )
 
 
