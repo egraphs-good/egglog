b3ae650ee23ab43c88daf696d023af10ecbebd7e
yihozhang <yihozhang@users.noreply.github.com>
2024-05-29 18:24:22 +0000

    deploy: 799c1be7e239870306529486e7585d84961888c1

---
+++ docs/egglog/util/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"type":["IndexMap","IndexSet"]};
---
+++ docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"egglog":["",[["ast",[],["desugar.rs","expr.rs","mod.rs","remove_globals.rs"]],["function",[],["binary_search.rs","index.rs","mod.rs","table.rs"]],["sort",[],["bool.rs","f64.rs","fn.rs","i64.rs","macros.rs","map.rs","mod.rs","rational.rs","set.rs","string.rs","unit.rs","vec.rs"]]],["actions.rs","constraint.rs","core.rs","extract.rs","gj.rs","lib.rs","serialize.rs","termdag.rs","typechecking.rs","unionfind.rs","util.rs","value.rs"]]\
+}');
+createSrcSidebar();
