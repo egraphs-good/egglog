4b5769ef8d249063527e0ffcc7d1c5bb6dbeb389
Yihong Zhang <yz489@cs.washington.edu>
2024-09-25 15:11:56 -0700

    get rid of unwanted deps

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
-    let build_date = chrono::Local::now().format("%Y-%m-%d");
+    let build_date = chrono::Utc::now().format("%Y-%m-%d");
     let version = env::var("CARGO_PKG_VERSION").unwrap();
     let full_version = format!("{}_{}_{}", version, build_date, git_hash);
     println!("cargo:rustc-env=FULL_VERSION={}", full_version);
