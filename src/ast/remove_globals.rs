fcb16e870dfe039b0d6042b1c2d606734334e150
oflatt <oflatt@gmail.com>
2024-10-08 16:48:38 -0700

    remove globals nondeterminism

--- src/ast/remove_globals.rs
+++ src/ast/remove_globals.rs
@@ -7,6 +7,7 @@
 
 use crate::*;
 use crate::{core::ResolvedCall, typechecking::FuncType};
+use indexmap::IndexMap;
 
 struct GlobalRemover<'a> {
     fresh: &'a mut SymbolGen,
@@ -142,7 +143,7 @@ impl<'a> GlobalRemover<'a> {
             } => {
                 // A map from the global variables in actions to their new names
                 // in the query.
-                let mut globals = HashMap::default();
+                let mut globals = IndexMap::new();
                 rule.head.clone().visit_exprs(&mut |expr| {
                     if let Some(resolved_var) = expr.get_global_var() {
                         let new_name = self.fresh.fresh(&resolved_var.name);
