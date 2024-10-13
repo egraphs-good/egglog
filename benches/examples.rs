fd2682e6eb207608f26a46aebdbec8725b03c077
Saul Shanabrook <s.shanabrook@gmail.com>
2024-10-12 21:14:27 -0400

    only benchmark non failing programs

--- benches/examples.rs
+++ benches/examples.rs
@@ -8,13 +8,22 @@ fn run_example(name: &Path) {
     let program = std::fs::read_to_string(&filename).unwrap();
     let mut egraph = EGraph::default();
     egraph.set_reserved_symbol("___".into());
-    let _ = egraph.parse_and_run_program(Some(filename), &program);
+    egraph
+        .parse_and_run_program(Some(filename), &program)
+        .unwrap();
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     for entry in glob::glob("tests/**/*.egg").unwrap() {
         let path = entry.unwrap().clone();
         let name = path.file_stem().unwrap().to_string_lossy().to_string();
+        if path
+            .to_string_lossy()
+            .to_string()
+            .contains("fail-typecheck")
+        {
+            continue;
+        }
         c.bench_with_input(BenchmarkId::new("example", &name), &path, |_b, f| {
             run_example(f)
         });
