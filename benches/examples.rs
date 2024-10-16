2fa06d9366193c81ae375ee8801d721d17ad945d
Saul Shanabrook <s.shanabrook@gmail.com>
2024-10-15 20:49:45 -0400

    fix typo

--- benches/examples.rs
+++ benches/examples.rs
@@ -11,7 +11,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     for entry in glob::glob("tests/**/*.egg").unwrap() {
         let path = entry.unwrap().clone();
         let path_string = path.to_string_lossy().to_string();
-        // Skip python_array_optimize since it is too slow and doesn't even reflect currenty python implementation
+        // Skip python_array_optimize since it is too slow and doesn't even reflect the current python implementation
         if path_string.contains("fail-typecheck") || path_string.contains("python_array_optimize") {
             continue;
         }
