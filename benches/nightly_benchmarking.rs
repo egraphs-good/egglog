83503365154d1d11685700cc7b81ba07a5d583b5
oliver <oflatt@gmail.com>
2025-03-17 11:46:59 -0700

    eggcc submission benchmarks
    add all of them
    
    move to eggccsubmission
    
    delete a bunch of the benchmarks, too many
    
    make nightly ci script
    
    try again
    
    try again
    
    just run raytrace for now, biggest one
    
    run pull request for now
    
    undo running on pr
    
    try again on PR
    
    try on push
    
    yay working
    
    on push again
    
    use walltime
    
    move sample size to nightly oops
    
    samples size has to be 10
    
    try enabling all of them
    
    nightly bench only at night
    
    move to other repo
    
    test on push
    
    rename to example_benchmarks again
    
    disable on push again

--- benches/nightly_benchmarking.rs
+++ benches/nightly_benchmarking.rs
@@ -1,16 +1,13 @@
 mod common;
-
-// TODO rename this file to something better after codspeed supports renaming
-
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 
 fn bench(c: &mut Criterion) {
-    common::criterion_benchmark(c, "tests/**/*.egg");
+    common::criterion_benchmark(c, "egglog-benchmarks/**/*.egg");
 }
 
 criterion_group!(
     name = benches;
-    config = Criterion::default();
+    config = Criterion::default().sample_size(10);
     targets = bench
 );
 criterion_main!(benches);
