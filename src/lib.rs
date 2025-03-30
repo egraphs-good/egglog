c219b3fd64b5ba84d7eeb1fa5172a47b559e6fd7
Alex Fischman <alexdfischman@gmail.com>
2025-03-29 21:38:01 -0700

    Implement Multiset (not unstable-multiset-map) and uncomment Function

--- src/lib.rs
+++ src/lib.rs
@@ -452,8 +452,8 @@ impl Default for EGraph {
         eg.type_info.add_presort::<MapSort>(span!()).unwrap();
         eg.type_info.add_presort::<SetSort>(span!()).unwrap();
         eg.type_info.add_presort::<VecSort>(span!()).unwrap();
-        // eg.type_info.add_presort::<FunctionSort>(span!()).unwrap();
-        // eg.type_info.add_presort::<MultiSetSort>(span!()).unwrap();
+        eg.type_info.add_presort::<FunctionSort>(span!()).unwrap();
+        eg.type_info.add_presort::<MultiSetSort>(span!()).unwrap();
 
         add_primitive!(&mut eg, "!=" = |a: #, b: #| -?> () {
             (a != b).then_some(())
