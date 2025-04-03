7160cced0ec8a0a960054cc2b209167826438ca3
oflatt <oflatt@gmail.com>
2025-04-03 11:16:52 -0700

    just run raytrace for now, biggest one

--- benches/nightly_benchmarking.rs
+++ benches/nightly_benchmarking.rs
@@ -2,7 +2,7 @@ mod common;
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 
 fn benchmark(c: &mut Criterion) {
-    common::criterion_benchmark(c, "benchmarks/**/*.egg");
+    common::criterion_benchmark(c, "benchmarks/**/raytrace.egg");
 }
 
 criterion_group!(benches, benchmark);
