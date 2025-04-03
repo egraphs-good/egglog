e8de4c0c1b9afc2b5c9d39c076126b2eae614715
oflatt <oflatt@gmail.com>
2025-04-03 12:25:52 -0700

    move sample size to nightly oops

--- benches/nightly_benchmarking.rs
+++ benches/nightly_benchmarking.rs
@@ -1,9 +1,13 @@
 mod common;
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 
-fn benchmark(c: &mut Criterion) {
+fn bench(c: &mut Criterion) {
     common::criterion_benchmark(c, "benchmarks/**/raytrace.egg");
 }
 
-criterion_group!(benches, benchmark);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(2);
+    targets = bench
+);
 criterion_main!(benches);
