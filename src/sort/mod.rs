2bd1081e4163dc72ab16a98e79878bed266a7f41
Alex Fischman <alexdfischman@gmail.com>
2025-06-01 16:56:16 -0700

    Cleanup

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -161,6 +161,9 @@ pub trait Presort {
     ) -> Result<ArcSort, TypeError>;
 }
 
+pub type MakeSort =
+    fn(typeinfo: &mut TypeInfo, name: Symbol, params: &[Expr]) -> Result<ArcSort, TypeError>;
+
 #[derive(Debug)]
 pub struct EqSort {
     pub name: Symbol,
@@ -189,9 +192,6 @@ impl Sort for EqSort {
         None
     }
 }
-
-pub type PreSort =
-    fn(typeinfo: &mut TypeInfo, name: Symbol, params: &[Expr]) -> Result<ArcSort, TypeError>;
 
 pub fn literal_sort(lit: &Literal) -> ArcSort {
     match lit {
