d8ffd573916926d07aab0b54f924778117c6b297
Eli Rosenthal <ezr@google.com>
2025-06-05 00:05:28 -0700

    hard-code 1 thread for benchmarks for now

--- benches/example_benchmarks.rs
+++ benches/example_benchmarks.rs
@@ -1,8 +1,12 @@
+use std::sync::Once;
+
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 use egglog::EGraph;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+static CONFIGURE_RAYON: Once = Once::new();
 
 fn run_example(filename: &str, program: &str, no_messages: bool) {
     let mut egraph = EGraph::default();
@@ -17,6 +21,12 @@ fn run_example(filename: &str, program: &str, no_messages: bool) {
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    CONFIGURE_RAYON.call_once(|| {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(1)
+            .build_global()
+            .unwrap();
+    });
     for entry in glob::glob("tests/**/*.egg").unwrap() {
         let path = entry.unwrap().clone();
         let path_string = path.to_string_lossy().to_string();
