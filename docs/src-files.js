388ad8a7f2be1b5daf96ba7fcd1ac98a36426847
Alex-Fischman <Alex-Fischman@users.noreply.github.com>
2025-06-06 05:34:40 +0000

    deploy: 1b5fe9099f10fdb13f795f7a8f4f79f36dfc7a1b

---
+++ docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["egglog",["",[["ast",[],["check_shadowing.rs","desugar.rs","expr.rs","mod.rs","parse.rs","remove_globals.rs"]],["function",[],["binary_search.rs","index.rs","mod.rs","table.rs"]],["sort",[],["bigint.rs","bigrat.rs","bool.rs","f64.rs","fn.rs","i64.rs","macros.rs","map.rs","mod.rs","multiset.rs","set.rs","string.rs","unit.rs","vec.rs"]]],["actions.rs","cli.rs","constraint.rs","core.rs","extract.rs","gj.rs","lib.rs","serialize.rs","termdag.rs","typechecking.rs","unionfind.rs","util.rs","value.rs"]]]\
+]'));
+createSrcSidebar();
