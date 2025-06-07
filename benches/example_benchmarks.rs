cb9678bb8a6ac406096b6dbb6283e848f6162a6b
Alex Fischman <alexdfischman@gmail.com>
2025-06-07 13:22:41 -0700

    Cleanup imports

--- benches/example_benchmarks.rs
+++ benches/example_benchmarks.rs
@@ -1,7 +1,6 @@
-mod common;
-
 // TODO rename this file to something better after codspeed supports renaming
 
+mod common;
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
