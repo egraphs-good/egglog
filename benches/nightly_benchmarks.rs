55e3046a91559e7c22dd7c8a51df1212a2937522
Alex Fischman <alexdfischman@gmail.com>
2025-06-07 11:20:52 -0700

    Add eggcc benchmarks

--- benches/nightly_benchmarks.rs
+++ benches/nightly_benchmarks.rs
@@ -1,16 +1,14 @@
-// TODO: rename this file to something better after codspeed supports renaming
-
 mod common;
 
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 
 fn bench(c: &mut Criterion) {
-    common::criterion_benchmark(c, "tests/**/*.egg");
+    common::criterion_benchmark(c, "egglog-benchmarks/**/*.egg");
 }
 
 criterion_group!(
     name = benches;
-    config = Criterion::default();
+    config = Criterion::default().sample_size(10);
     targets = bench
 );
 criterion_main!(benches);
