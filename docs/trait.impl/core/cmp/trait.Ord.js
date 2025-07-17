d940691b541ea10a9a4f2991b7b53180d0e1565e
saulshanabrook <saulshanabrook@users.noreply.github.com>
2025-07-17 17:26:36 +0000

    deploy: 12661d61eeba0e8b84d8584b7dd2674641b31fe5

---
+++ docs/trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["egglog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"egglog/ast/enum.Literal.html\" title=\"enum egglog::ast::Literal\">Literal</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"egglog/scheduler/struct.SchedulerId.html\" title=\"struct egglog::scheduler::SchedulerId\">SchedulerId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[524]}
