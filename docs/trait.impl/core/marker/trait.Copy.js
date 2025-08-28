3e1c60911a9cc8b4ccc93b012c9c1f9222c2b07d
saulshanabrook <saulshanabrook@users.noreply.github.com>
2025-08-28 22:05:12 +0000

    deploy: 0be495630546acffbd545ba60feb9302281ce95c

---
+++ docs/trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["egglog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"egglog/ast/enum.Change.html\" title=\"enum egglog::ast::Change\">Change</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"egglog/ast/enum.FunctionSubtype.html\" title=\"enum egglog::ast::FunctionSubtype\">FunctionSubtype</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"egglog/ast/enum.PrintFunctionMode.html\" title=\"enum egglog::ast::PrintFunctionMode\">PrintFunctionMode</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"egglog/scheduler/struct.SchedulerId.html\" title=\"struct egglog::scheduler::SchedulerId\">SchedulerId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1093]}
