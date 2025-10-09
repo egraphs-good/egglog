0e99f73794301813b55918d8fab3584a422c5a04
Saul Shanabrook <s.shanabrook@gmail.com>
2025-10-08 17:34:35 -0700

    Revert "Revert "Try switching to divan""
    0df3587f4ab5a34e0fccc5cb141da8fdb49554aa

--- benches/example_benchmarks.rs
+++ benches/example_benchmarks.rs
@@ -1,18 +1,12 @@
 // TODO rename this file to something better after codspeed supports renaming
 
 mod common;
-use codspeed_criterion_compat::{Criterion, criterion_group, criterion_main};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    common::benchmark_files_in_glob(c, "tests/**/*.egg");
+#[divan::bench(args = common::bench_cases("tests/**/*.egg"), sample_count = 10)]
+fn run_example_cases(case: &common::BenchCase) {
+    common::bench_case(case);
 }
 
-criterion_group!(
-    name = benches;
-    config = Criterion::default()
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(10))
-        .measurement_time(std::time::Duration::from_secs(1));
-    targets = criterion_benchmark
-);
-criterion_main!(benches);
+fn main() {
+    divan::main();
+}
