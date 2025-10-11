33c175a7813cb4e7fbda78e357d81d9c8657e29b
Saul Shanabrook <s.shanabrook@gmail.com>
2025-10-10 22:59:05 -0700

    Rename benchmark functions to make them shorter

--- benches/ci_benchmarking.rs
+++ benches/ci_benchmarking.rs
@@ -1,9 +1,7 @@
-// TODO rename this file to something better after codspeed supports renaming
-
 mod common;
 
 #[divan::bench(args = common::bench_cases("tests/**/*.egg"), sample_count = 10)]
-fn run_example_cases(case: &common::BenchCase) {
+fn tests(case: &common::BenchCase) {
     common::bench_case(case);
 }
 
--- benches/nightly_benchmarking.rs
+++ benches/nightly_benchmarking.rs
@@ -1,7 +1,7 @@
 mod common;
 
 #[divan::bench(args = common::bench_cases("egglog-benchmarks/**/*.egg"), sample_size = 10)]
-fn run_nightly_cases(case: &common::BenchCase) {
+fn benchmarks(case: &common::BenchCase) {
     common::bench_case(case);
 }
 
