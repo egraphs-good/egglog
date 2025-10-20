7f12370aca79805a73c053051049e800645fba47
Anjali Pal <anjalip@cs.washington.edu>
2025-10-20 10:09:54 -0700

    nightly updates

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -43,19 +43,22 @@ popd
 
 pushd $TOP_DIR
 cargo run --release --bin egglog -- tests/
-cp "summary.json" "$NIGHTLY_DIR/output/data"
+# Put the json data in a JS object for consumption by frontend
+(echo "var data = "; cat "$NIGHTLY_DIR/data/summary.json") > "$NIGHTLY_DIR/data/summary.js"
 popd
 
 
 # Update HTML index page.
 cp "$RESOURCE_DIR/web"/* "$NIGHTLY_DIR/output"
 
-# # This is the uploading part, copied directly from Herbie's nightly script.
-# DIR="$NIGHTLY_DIR/output"
-# B=$(git rev-parse --abbrev-ref HEAD)
-# C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
-# RDIR="$(date +%s):$(hostname):$B:$C"
+# This is the uploading part, copied directly from Herbie's nightly script.
+DIR="$NIGHTLY_DIR/output"
+B=$(git rev-parse --abbrev-ref HEAD)
+C=$(git rev-parse HEAD | sed 's/\(..........\).*/\1/')
+RDIR="$(date +%s):$(hostname):$B:$C"
 
-# # Upload the artifact!
-# nightly-results publish --name "$RDIR" "$DIR"
-cd "$NIGHTLY_DIR/output" && python3 -m http.server 8002
+# Upload the artifact!
+nightly-results publish --name "$RDIR" "$DIR"
+
+# For local dev
+# cd "$NIGHTLY_DIR/output" && python3 -m http.server 8002
