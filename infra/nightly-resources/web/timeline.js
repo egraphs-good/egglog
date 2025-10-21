44c6cc326aaeef7f54d8813b76c5a4e37070cfcf
Anjali Pal <anjalip@cs.washington.edu>
2025-10-21 14:51:42 -0700

    plot total/avg/max

--- infra/nightly-resources/web/timeline.js
+++ infra/nightly-resources/web/timeline.js
@@ -1,64 +1,84 @@
-const RUN_CMDS = ["run", "run-schedule"];
-const ACT_CMDS = ["extract"];
-
+var AGGREGATED_DATA = {};
 // Top-level load function for the timeline page.
 function load_timeline() {
+  // list.json contains a list of all the json files (one for each egg benchmark)
   fetch("data/list.json")
     .then((response) => response.json())
     .then(buildGlobalData);
 }
 
 function buildGlobalData(names) {
+  // map from filename to timeline data
   const allData = {};
+
   const promises = names.map((name) =>
     fetch(`data/${name}`)
       .then((r) => r.json())
+      // @Noah -- why does each data file contain a singleton list?
+      // Would there ever be multiple elements? If not, should we make it just the object, not a list?
       .then((d) => (allData[name] = d[0]))
   );
 
   Promise.all(promises).then(() => {
-    const aggregated = aggregate(allData);
-    plot(aggregated);
-  });
-}
+    const RUN_CMDS = ["run", "run-schedule"];
+    const EXT_CMDS = ["extract"];
+    Object.keys(allData).forEach((filename) => {
+      times = {
+        runs: [],
+        exts: [],
+        others: [],
+      };
+      const entries = allData[filename].evts;
+      entries.forEach((entry) => {
+        // compute event duration in ms from recorded seconds and nanoseconds
+        // @Noah- should we just record ms directly?
+        const ms = toMs(entry.total_time);
+        const cmd = entry.cmd;
 
-function aggregate(data) {
-  const aggregated = {};
-  Object.keys(data).forEach((filename) => {
-    times = {
-      runs: [],
-      acts: [],
-      others: [],
-    };
-    const entries = data[filename].evts;
-    entries.forEach((entry) => {
-      const ms = toMs(entry.total_time);
-      const cmd = entry.cmd;
-      if (RUN_CMDS.includes(cmd)) {
-        times.runs.push(ms);
-      } else if (ACT_CMDS.includes(cmd)) {
-        times.acts.push(ms);
-      } else {
-        times.others.push(ms);
-      }
+        // group commands by type (run, extract, other)
+        if (RUN_CMDS.includes(cmd)) {
+          times.runs.push(ms);
+        } else if (EXT_CMDS.includes(cmd)) {
+          times.exts.push(ms);
+        } else {
+          times.others.push(ms);
+        }
+      });
+      AGGREGATED_DATA[filename] = times;
     });
-    aggregated[filename] = times;
-  });
 
-  return aggregated;
+    plot();
+  });
 }
 
 function toMs(duration) {
   return duration.secs * 1e3 + duration.nanos / 1e6;
 }
 
-function plot(data) {
+function aggregate(times, mode) {
+  switch (mode) {
+    case "average":
+      return times.reduce((a, b) => a + b) / times.length;
+
+    case "total":
+      return times.reduce((a, b) => a + b);
+
+    case "max":
+      return Math.max(...times);
+
+    default:
+      console.warn("Unknown selection:", mode);
+      return 0;
+  }
+}
+
+function plot() {
   const ctx = document.getElementById("chart").getContext("2d");
 
-  const points = Object.values(data).map((entry) => {
-    const runTime = entry.runs.reduce((a, b) => a + b);
-    const extractTime = entry.acts.reduce((a, b) => a + b);
-    return { x: runTime, y: extractTime };
+  const mode = document.querySelector('input[name="mode"]:checked').value;
+
+  const points = Object.values(AGGREGATED_DATA).map((entry) => {
+    return { x: aggregate(entry.runs, mode), y: aggregate(entry.exts, mode) };
   });
 
   new Chart(ctx, {
