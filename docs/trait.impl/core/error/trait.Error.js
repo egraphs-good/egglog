9806bd613ba15b51b6d9fea6716b413fe28c36c8
yihozhang <yihozhang@users.noreply.github.com>
2025-10-28 20:01:51 +0000

    deploy: ef90b97de1f5e7778186439b8fb0549179f82a45

---
+++ docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["egglog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"egglog/enum.Error.html\" title=\"enum egglog::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"egglog/enum.TypeError.html\" title=\"enum egglog::TypeError\">TypeError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"egglog/ast/struct.ParseError.html\" title=\"struct egglog::ast::ParseError\">ParseError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"egglog/struct.NotFoundError.html\" title=\"struct egglog::NotFoundError\">NotFoundError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1028]}
