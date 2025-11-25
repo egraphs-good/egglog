71896dc601f48ac31c12cf0f16f0be076dfa4d15
saulshanabrook <saulshanabrook@users.noreply.github.com>
2025-11-25 01:41:20 +0000

    deploy: c4025d13c5c4292c553f20b8b9b550bdefbfd307

---
+++ docs/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["egglog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"egglog/scheduler/struct.SchedulerId.html\" title=\"struct egglog::scheduler::SchedulerId\">SchedulerId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[304]}
