0b4e939f541238408d3d8bff13b95582558560ea
oflatt <oflatt@users.noreply.github.com>
2025-12-02 00:18:22 +0000

    deploy: 33f79948ca25ec170b585515fb47809368ad2897

---
+++ docs/trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["egglog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"egglog/enum.TypeError.html\" title=\"enum egglog::TypeError\">TypeError</a>&gt; for <a class=\"enum\" href=\"egglog/enum.Error.html\" title=\"enum egglog::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"egglog/ast/struct.ParseError.html\" title=\"struct egglog::ast::ParseError\">ParseError</a>&gt; for <a class=\"enum\" href=\"egglog/enum.Error.html\" title=\"enum egglog::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"egglog/struct.NotFoundError.html\" title=\"struct egglog::NotFoundError\">NotFoundError</a>&gt; for <a class=\"enum\" href=\"egglog/enum.Error.html\" title=\"enum egglog::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1083]}
