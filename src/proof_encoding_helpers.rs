0ce8d7d811ebf8344b5afd997a23cdfc6076c0ff
oflatt <oflatt@gmail.com>
2025-12-27 17:20:55 -0700

    renames

---
+++ src/proof_encoding_helpers.rs
@@ -0,0 +1,257 @@
+use crate::{
+    ast::{Command, Fact, Schedule},
+    proof_encoding::TermState,
+    util::{FreshGen, HashMap, SymbolGen},
+};
+
+#[derive(Clone)]
+pub(crate) struct EncodingNames {
+    pub(crate) proof_list_sort: String,
+    pub(crate) ast_sort: String,
+    pub(crate) proof_datatype: String,
+    pub(crate) fiat_constructor: String,
+    pub(crate) rule_constructor: String,
+    pub(crate) merge_fn_constructor: String,
+    pub(crate) eq_trans_constructor: String,
+    pub(crate) eq_sym_constructor: String,
+    pub(crate) congr_constructor: String,
+    /// For a given function symbol, the name of the function that converts to the AST type.
+    pub(crate) sort_to_ast_constructor: HashMap<String, String>,
+    pub(crate) fn_to_term_sort: HashMap<String, String>,
+    pub(crate) uf_proof_name: HashMap<String, String>,
+    pub(crate) single_parent_ruleset_name: String,
+}
+
+/// Packages proof information for instrumenting actions.
+/// We may not know yet what terms we are instrumenting, so Rule and Fiat leave that information to be filled in later.
+pub(crate) enum Justification {
+    Rule(String, String), // rule name and proof list
+    Fiat,
+    Proof(String), // existing proof
+}
+
+impl EncodingNames {
+    pub(crate) fn new(symbol_gen: &mut SymbolGen) -> Self {
+        Self {
+            proof_list_sort: symbol_gen.fresh("ProofList"),
+            ast_sort: symbol_gen.fresh("Ast"),
+            proof_datatype: symbol_gen.fresh("Proof"),
+            fiat_constructor: symbol_gen.fresh("Fiat"),
+            rule_constructor: symbol_gen.fresh("Rule"),
+            merge_fn_constructor: symbol_gen.fresh("Merge"),
+            eq_trans_constructor: symbol_gen.fresh("Trans"),
+            eq_sym_constructor: symbol_gen.fresh("Sym"),
+            congr_constructor: symbol_gen.fresh("Congr"),
+            sort_to_ast_constructor: HashMap::default(),
+            fn_to_term_sort: HashMap::default(),
+            uf_proof_name: HashMap::default(),
+            single_parent_ruleset_name: "single_parent".to_string(),
+        }
+    }
+}
+
+impl<'a> TermState<'a> {
+    pub(crate) fn uf_name(&mut self, sort: &str) -> String {
+        if let Some(name) = self.egraph.proof_state.uf_parent.get(sort) {
+            name.clone()
+        } else {
+            let fresh_name = self.egraph.parser.symbol_gen.fresh(&format!("UF_{}", sort));
+            self.egraph
+                .proof_state
+                .uf_parent
+                .insert(sort.to_string(), fresh_name.clone());
+            fresh_name
+        }
+    }
+
+    pub(crate) fn uf_proof_name(&mut self, sort: &str) -> String {
+        if let Some(name) = self.egraph.proof_state.proof_names.uf_proof_name.get(sort) {
+            name.clone()
+        } else {
+            let fresh_name = self
+                .egraph
+                .parser
+                .symbol_gen
+                .fresh(&format!("{}UFProof", sort));
+            self.egraph
+                .proof_state
+                .proof_names
+                .uf_proof_name
+                .insert(sort.to_string(), fresh_name.clone());
+            fresh_name
+        }
+    }
+
+    pub(crate) fn parse_program(&mut self, input: &str) -> Vec<Command> {
+        eprintln!("Parsing program:\n{}", input);
+        self.egraph.parser.ensure_no_reserved_symbols = false;
+        let res = self.egraph.parser.get_program_from_string(None, input);
+        self.egraph.parser.ensure_no_reserved_symbols = true;
+
+        res.unwrap()
+    }
+
+    // TODO add these as fresh names
+    pub(crate) fn parent_direct_ruleset_name(&self) -> String {
+        "parent".to_string()
+    }
+
+    pub(crate) fn rebuilding_ruleset_name(&self) -> String {
+        "rebuilding".to_string()
+    }
+
+    pub(crate) fn rebuilding_cleanup_ruleset_name(&self) -> String {
+        "rebuilding_cleanup".to_string()
+    }
+
+    pub(crate) fn term_header(&mut self) -> Vec<Command> {
+        let str = format!(
+            "(ruleset {})
+             (ruleset {})
+             (ruleset {})
+             (ruleset {})
+             (ruleset {})",
+            self.parent_direct_ruleset_name(),
+            self.proof_names().single_parent_ruleset_name,
+            self.rebuilding_ruleset_name(),
+            self.rebuilding_cleanup_ruleset_name(),
+            self.delete_subsume_ruleset_name(),
+        );
+        self.parse_program(&str)
+    }
+
+    pub(crate) fn parse_schedule(&mut self, input: String) -> Schedule {
+        self.egraph.parser.ensure_no_reserved_symbols = false;
+        let res = self.egraph.parser.get_schedule_from_string(None, &input);
+        self.egraph.parser.ensure_no_reserved_symbols = true;
+        res.unwrap()
+    }
+
+    pub(crate) fn parse_facts(&mut self, input: &[String]) -> Vec<Fact> {
+        self.egraph.parser.ensure_no_reserved_symbols = false;
+        let res = input
+            .iter()
+            .map(|f| self.egraph.parser.get_fact_from_string(None, f).unwrap())
+            .collect();
+        self.egraph.parser.ensure_no_reserved_symbols = true;
+        res
+    }
+
+    // Each function/constructor gets a view table, the canonicalized e-nodes to accelerate e-matching.
+    pub(crate) fn view_name(&self, name: &str) -> String {
+        format!("{}View", name)
+    }
+
+    pub(crate) fn to_delete_name(&self, name: &str) -> String {
+        format!("to_delete_{}", name)
+    }
+
+    pub(crate) fn subsumed_name(&self, name: &str) -> String {
+        format!("to_subsume_{}", name)
+    }
+
+    pub(crate) fn delete_subsume_ruleset_name(&self) -> String {
+        "delete_subsume_ruleset".to_string()
+    }
+
+    pub(crate) fn fiat_justification(&mut self) -> String {
+        if !self.egraph.proof_state.proofs_enabled {
+            return String::new();
+        }
+        let names = self.proof_names();
+        format!("({})", names.fiat_constructor)
+    }
+
+    pub(crate) fn proof_names(&self) -> &EncodingNames {
+        &self.egraph.proof_state.proof_names
+    }
+
+    /// Given a function name, returns the name of the AST constructor for that function's sort.
+    pub(crate) fn fname_to_ast_name(&self, fname: &str) -> &str {
+        let fn_sort = self
+            .proof_names()
+            .fn_to_term_sort
+            .get(fname)
+            .unwrap_or_else(|| panic!("Function {} has no recorded sort", fname))
+            .clone();
+        self.proof_names()
+            .sort_to_ast_constructor
+            .get(&fn_sort)
+            .unwrap_or_else(|| {
+                panic!(
+                    "Function {}'s sort {} has no recorded AST constructor",
+                    fname, fn_sort
+                )
+            })
+            
+    }
+
+    /// A view proof for functions proves ... = t by some justification, where t is the term of the view row.
+    /// A constructor view is more complex, representing f(c1, c2, c3, t_r) where t_r is a representative.
+    /// A proof for a view proves that t_r = f(c1, c2, c3).
+    pub(crate) fn view_proof_name(&self, name: &str) -> String {
+        format!("{}ViewProof", name)
+    }
+
+    pub(crate) fn fresh_var(&mut self) -> String {
+        self.egraph.parser.symbol_gen.fresh("v")
+    }
+
+    pub(crate) fn proof_header(&mut self) -> String {
+        let (
+            proof_list_sort,
+            ast_sort,
+            proof_datatype,
+            fiat_constructor,
+            rule_constructor,
+            merge_fn_constructor,
+            eq_trans_constructor,
+            eq_sym_constructor,
+            congr_constructor,
+        ) = {
+            let names = self.proof_names();
+            (
+                names.proof_list_sort.clone(),
+                names.ast_sort.clone(),
+                names.proof_datatype.clone(),
+                names.fiat_constructor.clone(),
+                names.rule_constructor.clone(),
+                names.merge_fn_constructor.clone(),
+                names.eq_trans_constructor.clone(),
+                names.eq_sym_constructor.clone(),
+                names.congr_constructor.clone(),
+            )
+        };
+
+        format!(
+            "
+(sort {proof_list_sort})
+(sort {ast_sort}) ;; wrap sorts in this for proofs
+(sort {proof_datatype})
+
+;; TODO remove this
+(constructor TODOList () {proof_list_sort})
+;; A proof shows that two ASTs are equal.
+
+;; Fiat justification for globals
+(constructor {fiat_constructor} ({ast_sort} {ast_sort}) {proof_datatype})
+;; name of rule and one proof per fact in the query
+(constructor {rule_constructor} (String {proof_list_sort} {ast_sort} {ast_sort}) {proof_datatype})
+
+;; merge function justification- name of function and two proofs for the two terms being merged
+(constructor {merge_fn_constructor} (String {proof_datatype} {proof_datatype}) {proof_datatype})
+
+;; transitivity of equality proofs
+(constructor {eq_trans_constructor} ({proof_datatype} {proof_datatype}) {proof_datatype})
+
+;; symmetry of equality proofs
+(constructor  {eq_sym_constructor} ({proof_datatype}) {proof_datatype})
+;; given a proof that t1 = f(..., c1, ...) and a term f(..., c2, ...)
+;; and the child index of c1 in the term f(..., c1, ...)
+;; and a proof that c1 = c2,
+;; produces a justification that t1 = f(..., c2, ...)
+(constructor  {congr_constructor} ({proof_datatype} i64 {proof_datatype}) {proof_datatype})
+                "
+        )
+    }
+}
