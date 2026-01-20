0113b06b75c88af81408823dbbd45b34b90058ed
Eli Rosenthal <ezr@cs.brown.edu>
2026-01-19 23:36:21 -0800

    Add cyclic rebuild microbenchmark

---
+++ egglog-bridge/benches/math_partition_refine.rs
@@ -0,0 +1,289 @@
+use divan::Bencher;
+use egglog_bridge::{
+    ColumnTy, DefaultVal, EGraph, FunctionConfig, MergeFn, RuleId, add_expressions, define_rule,
+};
+use mimalloc::MiMalloc;
+use num_rational::Rational64;
+use std::sync::Once;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+const N: usize = 5;
+
+fn main() {
+    divan::main();
+}
+
+fn init_rayon() {
+    static INIT: Once = Once::new();
+    INIT.call_once(|| {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(1)
+            .build_global()
+            .expect("rayon thread pool should initialize once");
+    });
+}
+
+fn setup_math(egraph: &mut EGraph) -> Vec<RuleId> {
+    let rational_ty = egraph.base_values_mut().register_type::<Rational64>();
+    let string_ty = egraph.base_values_mut().register_type::<&'static str>();
+
+    let diff = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "diff".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+    let integral = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "integral".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let add = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "add".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+    let sub = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "sub".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let mul = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "mul".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let div = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "div".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let pow = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "pow".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let ln = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "ln".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let sqrt = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "sqrt".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let sin = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "sin".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let cos = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Id, ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "cos".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let rat = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Base(rational_ty), ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "rat".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let var = egraph.add_table(FunctionConfig {
+        schema: vec![ColumnTy::Base(string_ty), ColumnTy::Id],
+        default: DefaultVal::FreshId,
+        merge: MergeFn::UnionId,
+        name: "var".into(),
+        can_subsume: false,
+        row_id: false,
+    });
+
+    let zero = egraph.base_value_constant(Rational64::new(0, 1));
+    let one = egraph.base_value_constant(Rational64::new(1, 1));
+    let neg1 = egraph.base_value_constant(Rational64::new(-1, 1));
+    let two = egraph.base_value_constant(Rational64::new(2, 1));
+    let rules = vec![
+        define_rule! {
+            [egraph] ((-> (add x y) id)) => ((set (add y x) id))
+        },
+        define_rule! {
+            [egraph] ((-> (mul x y) id)) => ((set (mul y x) id))
+        },
+        define_rule! {
+            [egraph] ((-> (add x (add y z)) id)) => ((set (add (add x y) z) id))
+        },
+        define_rule! {
+            [egraph] ((-> (mul x (mul y z)) id)) => ((set (mul (mul x y) z) id))
+        },
+        define_rule! {
+            [egraph] ((-> (sub x y) id)) => ((set (add x (mul (rat {neg1.clone()}) y)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (add a (rat {zero.clone()})) id)) => ((union a id))
+        },
+        define_rule! {
+            [egraph] ((-> (rat {zero.clone()}) z_id) (-> (mul a z_id) id))
+                    => ((union id z_id))
+        },
+        define_rule! {
+            [egraph] ((-> (mul a (rat {one.clone()})) id)) => ((union a id))
+        },
+        define_rule! {
+            [egraph] ((-> (sub x x) id)) => ((union id (rat {zero})))
+        },
+        define_rule! {
+            [egraph] ((-> (mul x (add b c)) id)) => ((set (add (mul x b) (mul x c)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (add (mul x a) (mul x b)) id)) => ((set (mul x (add a b)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (mul (pow a b) (pow a c)) id)) => ((set (pow a (add b c)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (pow x (rat {one.clone()})) id)) => ((union x id))
+        },
+        define_rule! {
+            [egraph] ((-> (pow x (rat {two})) id)) => ((set (mul x x) id))
+        },
+        define_rule! {
+            [egraph] ((-> (diff x (add a b)) id)) => ((set (add (diff x a) (diff x b)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (diff x (mul a b)) id)) => ((set (add (mul a (diff x b)) (mul b (diff x a))) id))
+        },
+        define_rule! {
+            [egraph] ((-> (diff x (sin x)) id)) => ((set (cos x) id))
+        },
+        define_rule! {
+            [egraph] ((-> (diff x (cos x)) id)) => ((set (mul (rat {neg1.clone()}) (sin x)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (rat {one}) x) id)) => ((union id x))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (cos x) x) id)) => ((set (sin x) id))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (sin x) x) id)) => ((set (mul (rat {neg1}) (cos x)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (add f g) x) id)) => ((set (add (integral f x) (integral g x)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (sub f g) x) id)) => ((set (sub (integral f x) (integral g x)) id))
+        },
+        define_rule! {
+            [egraph] ((-> (integral (mul a b) x) id))
+            => ((set (sub (mul a (integral b x))
+                          (integral (mul (diff x a) (integral b x)) x)) id))
+        },
+    ];
+
+    let one = egraph.base_values().get(Rational64::new(1, 1));
+    let two = egraph.base_values().get(Rational64::new(2, 1));
+    let three = egraph.base_values().get(Rational64::new(3, 1));
+    let seven = egraph.base_values().get(Rational64::new(7, 1));
+    let x_str = egraph.base_values_mut().get::<&'static str>("x");
+    let y_str = egraph.base_values_mut().get::<&'static str>("y");
+    let five_str = egraph.base_values_mut().get::<&'static str>("five");
+    add_expressions! {
+        [egraph]
+
+        (integral (ln (var x_str)) (var x_str))
+        (integral (add (var x_str) (cos (var x_str))) (var x_str))
+        (integral (mul (cos (var x_str)) (var x_str)) (var x_str))
+        (diff (var x_str)
+            (add (rat one) (mul (rat two) (var x_str))))
+        (diff (var x_str)
+            (sub (pow (var x_str) (rat three)) (mul (rat seven) (pow (var x_str) (rat two)))))
+        (add
+            (mul (var y_str) (add (var x_str) (var y_str)))
+            (sub (add (var x_str) (rat two)) (add (var x_str) (var x_str))))
+        (div (rat one)
+             (sub (div (add (rat one) (sqrt (var five_str))) (rat two))
+                  (div (sub (rat one) (sqrt (var five_str))) (rat two))))
+    }
+
+    rules
+}
+
+fn run_iterations(egraph: &mut EGraph, rules: &[RuleId], n: usize, cyclic: bool) {
+    for _ in 0..n {
+        let _ = egraph.run_rules(rules).expect("rules should run");
+        if cyclic {
+            let _ = egraph
+                .run_hash_partition_refinement()
+                .expect("partition refinement should run");
+        }
+    }
+}
+
+fn bench_math(bench: Bencher, cyclic: bool) {
+    init_rayon();
+    bench.bench(|| {
+        let mut egraph = if cyclic {
+            EGraph::with_partition_refinement()
+        } else {
+            EGraph::default()
+        };
+        let rules = setup_math(&mut egraph);
+        run_iterations(&mut egraph, &rules, N, cyclic);
+    });
+}
+
+#[divan::bench(sample_count = 5)]
+fn math_default(bench: Bencher) {
+    bench_math(bench, false);
+}
+
+#[divan::bench(sample_count = 5)]
+fn math_partition_refine(bench: Bencher) {
+    bench_math(bench, true);
+}
