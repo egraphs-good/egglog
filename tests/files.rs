2a86610175eba78bc79459a22c27dbd541e938ef
oflatt <oflatt@gmail.com>
2026-02-19 15:41:04 -0800

    remove 51

--- tests/files.rs
+++ tests/files.rs
@@ -240,11 +240,12 @@ impl Run {
         {
             // Skip tests with known non-deterministic output
             let filename = self.path.file_stem().unwrap().to_string_lossy();
-            const SKIP_PATTERNS: [&str; 4] = [
+            const SKIP_PATTERNS: [&str; 5] = [
                 "extract-vec-bench",
                 "python_array_optimize",
                 "stresstest_large_expr",
                 "towers-of-hanoi",
+                "taylor51",
             ];
             if SKIP_PATTERNS.iter().any(|pat| filename.contains(pat)) {
                 return true;
