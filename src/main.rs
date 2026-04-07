af764e80f1248a17261e0d3a72f3b98eacab2b79
Haobin Ni <haobin.ni@gmail.com>
2026-04-07 15:54:26 -0700

    Poach main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use egglog::EGraph;
+mod poach;
+use poach::poach;
 
 #[cfg(feature = "bin")]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 fn main() {
-    egglog::cli(EGraph::default());
+    poach();
 }
---
+++ src/poach.rs
@@ -0,0 +1,18 @@
+pub fn poach (/* TODO */) {
+    // parse cli arguments
+    // split into the cases
+    // handle report IO
+}
+
+fn train() {
+    //TODO
+}
+
+fn serve() {
+    //TODO
+}
+
+fn fine_tune() {
+    //TODO
+}
+
