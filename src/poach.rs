3ee202aa914d19fe9ab651d63829d788c8902215
Haobin Ni <haobin.ni@gmail.com>
2026-04-09 12:53:38 -0700

    Reasonable CLI arguments

--- src/poach.rs
+++ src/poach.rs
@@ -1,50 +1,128 @@
-use clap::{Parser, ValueEnum};
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
 
 
-#[derive(Clone, Copy, ValueEnum)]
-enum Modes {
-    Train,
-    Serve,
-    FineTune,
-    Test,
-}
-
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(version, about)]
-struct Args {
-    mode: Modes,
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-pub fn poach (/* TODO */) {
-    let args = Args::parse();
-    match args.mode {
-        Modes::Train => {
-            train();
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Produce a model from a training set
+    Train(TrainArgs),
+    /// Use a model to process egglog programs
+    Serve(ServeArgs),
+    /// Update a model with new input-output pairs
+    FineTune(FineTuneArgs),
+    /// TEST
+    Test(TestArgs),
+}
+
+#[derive(Debug, Args)]
+struct TrainArgs {
+    /// If true, prints statistics to stderr
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Requires a directory
+    training_set_dir: PathBuf,
+
+    /// Requires a file
+    output_model_file: PathBuf,
+}
+
+#[derive(Debug, Args)]
+struct ServeArgs {
+    /// If true, prints statistics to stderr
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Requires a file
+    model_file: PathBuf,
+
+    /// None (default): Streaming mode
+    ///   reads input from stdin
+    ///   terminate when EOF, which is dynamic
+    ///   prints output to stdout
+    #[command(subcommand)]
+    serve_command: Option<ServeCommands>,
+}
+
+/// More subtle distinctions for the serve API
+/// As of Apr 9, 26, we are not there yet
+#[derive(Debug, Subcommand)]
+enum ServeCommands {
+    /// Single File input:
+    ///   reads a single .egg file
+    ///   which means it is closed
+    ///   prints output to stdout
+    Single {input_file: PathBuf},
+    /// Batch input:
+    ///   reads all .egg files in the input directory
+    ///   writes outputs files to the output directory
+    ///   the order of the input files should not matter
+    ///   this means the model only needs to be loaded once for all
+    Batch {input_dir: PathBuf, output_dir: PathBuf},
+}
+
+#[derive(Debug, Args)]
+struct FineTuneArgs {
+    /// If true, prints statistics to stderr
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Requires a file
+    input_model_file: PathBuf,
+
+    /// Requires two folders
+    /// Really should be a list of pairs instead of a pair of lists
+    /// For now, assumes the filename would relate the input to the output
+    input_dir: PathBuf,
+    output_dir: PathBuf,
+
+    /// Requires a file
+    output_model_file: PathBuf,
+}
+
+#[derive(Debug, Args)]
+struct TestArgs{
+}
+
+pub fn poach () {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Train(arg) => {
+            train(arg);
         }
-        Modes::Serve => {
-            serve();
+        Commands::Serve(arg) => {
+            serve(arg);
         }
-        Modes::FineTune => {
-            fine_tune();
+        Commands::FineTune(arg) => {
+            fine_tune(arg);
         }
-        Modes::Test => {
-            println!("test()");
+        Commands::Test(arg) => {
+            println!("test({:?})", arg);
         }
     }
     // TODO handle report IO
 }
 
-fn train() {
-    println!("train()");
+fn train(arg : TrainArgs) {
+    println!("train({:?})", arg);
     //TODO
 }
 
-fn serve() {
-    println!("serve()");
+fn serve(arg: ServeArgs) {
+    println!("serve({:?})", arg);
     //TODO
 }
 
-fn fine_tune() {
-    println!("fine_tune()");
+fn fine_tune(arg: FineTuneArgs) {
+    println!("fine_tune({:?})", arg);
     //TODO
 }
