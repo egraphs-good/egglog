64b51db9befae8a3ef87e217236e545e84b42c64
Haobin Ni <haobin.ni@gmail.com>
2026-04-09 11:38:16 -0700

    Remove wasm, fiddling with Cargo.toml

--- src/poach.rs
+++ src/poach.rs
@@ -1,18 +1,50 @@
+use clap::{Parser, ValueEnum};
+
+
+#[derive(Clone, Copy, ValueEnum)]
+enum Modes {
+    Train,
+    Serve,
+    FineTune,
+    Test,
+}
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    mode: Modes,
+}
+
 pub fn poach (/* TODO */) {
-    // parse cli arguments
-    // split into the cases
-    // handle report IO
+    let args = Args::parse();
+    match args.mode {
+        Modes::Train => {
+            train();
+        }
+        Modes::Serve => {
+            serve();
+        }
+        Modes::FineTune => {
+            fine_tune();
+        }
+        Modes::Test => {
+            println!("test()");
+        }
+    }
+    // TODO handle report IO
 }
 
 fn train() {
+    println!("train()");
     //TODO
 }
 
 fn serve() {
+    println!("serve()");
     //TODO
 }
 
 fn fine_tune() {
+    println!("fine_tune()");
     //TODO
 }
-
--- wasm-example/run.js
+++
@@ -1,7 +0,0 @@
-let { run } = require("./pkg/");
-
-if (run() !== 0) {
-  throw new Error("run() should return 0");
-}
-
-console.log("Successfully ran egglog program");
