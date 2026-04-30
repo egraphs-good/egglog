c9b9286d3d51b7f4320f489940732f0f7e011dd5
Yihong Zhang <yz489@cs.washington.edu>
2026-04-30 01:32:08 -0700

    nits

--- core-relations/src/free_join/execute.rs
+++ core-relations/src/free_join/execute.rs
@@ -763,12 +763,13 @@ impl BindingInfo {
     /// Initializes the atom-related metadata in the [`BindingInfo`].    
     fn insert_subset(&mut self, atom: AtomId, subset: Subset) {
         if let Some(slot) = self.subsets.get_mut(atom)
-            && let Some(node) = Arc::get_mut(slot) {
-                node.cached_subsets.take();
-                node.cached_children.take();
-                node.subset = subset;
-                return;
-            }
+            && let Some(node) = Arc::get_mut(slot)
+        {
+            node.cached_subsets.take();
+            node.cached_children.take();
+            node.subset = subset;
+            return;
+        }
         self.subsets.insert(atom, Arc::new(TrieNode::new(subset)));
     }
 
